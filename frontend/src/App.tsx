6945aedd54cd0d991fe8f67f635b22dba341407b
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-01-22 22:52:54 +0330

    feat: install and configure shadcn-ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div>
       <h1 className="bg-rose-500 text-3xl">Test TailwindCSS</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
